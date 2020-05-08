3183d06673b3cfcb98746d05dff1aaa273eab07c
Bruno Malvestuto <bruno.malvestuto@qstream.com>
2020-05-08 15:02:11 +0100

    Auth.generate_password_reset_code/3 function added.

--- lib/auth.ex
+++ lib/auth.ex
@@ -27,21 +27,21 @@ defmodule FirebaseAdminEx.Auth do
     do: get_user(:email, email, client_email)
 
   defp get_user(key, value, client_email),
-    do: do_request("getAccountInfo", %{key => value}, client_email)
+    do: do_request!("getAccountInfo", %{key => value}, client_email)
 
   @doc """
   Change an existing user's email
   """
   @spec change_user_email(String.t(), String.t(), String.t() | nil) :: tuple()
   def change_user_email(uid, email, client_email \\ nil),
-    do: do_request("setAccountInfo", %{localId: uid, email: email}, client_email)
+    do: do_request!("setAccountInfo", %{localId: uid, email: email}, client_email)
 
   @doc """
   Delete an existing user by UID
   """
   @spec delete_user(String.t(), String.t() | nil) :: tuple()
   def delete_user(uid, client_email \\ nil),
-    do: do_request("deleteAccount", %{localId: uid}, client_email)
+    do: do_request!("deleteAccount", %{localId: uid}, client_email)
 
   # TODO: Add other commands:
   # list_users
@@ -58,19 +58,35 @@ defmodule FirebaseAdminEx.Auth do
         client_email \\ nil
       ),
       do:
-        do_request(
+        do_request!(
           "signupNewUser",
           %{:email => email, :password => password, :returnSecureToken => true},
           client_email
         )
 
   @doc """
-  Generates the email action link for sign-in flows, using the action code settings provided
   """
   @spec generate_sign_in_with_email_link(ActionCodeSettings.t(), String.t(), String.t()) :: tuple()
   def generate_sign_in_with_email_link(action_code_settings, client_email, project_id) do
     with {:ok, action_code_settings} <- ActionCodeSettings.validate(action_code_settings) do
-      do_request("accounts:sendOobCode", action_code_settings, client_email, project_id)
+      do_request!("accounts:sendOobCode", action_code_settings, client_email, project_id)
+    end
+  end
+
+  @doc ~S"""
+  Generate a password reset code
+  """
+  @spec generate_password_reset_code(ActionCodeSettings.t(), client_email :: String.t(), project_id :: String.t()) :: {:ok, String.t()} | {:error, reason :: String.t()}
+  def generate_password_reset_code(action_code_settings, client_email \\ nil, project_id \\ nil) do
+    with {_stage, {:ok, action_code_settings}} <- {"validate", ActionCodeSettings.validate(action_code_settings)},
+         {_stage, {:ok, encoded_json}}         <- {"make_request",  do_request("accounts:sendOobCode", %{action_code_settings | requestType: "PASSWORD_RESET", returnOobLink: true }, client_email, project_id)},
+         {_stage, {:ok, response}}             <- {"parse_response", Jason.decode(encoded_json)},
+         {_stage, {:ok, link}}                 <- {"read_code_from_response", Map.fetch(response, "oobLink")} do
+      link
+      |> URI.parse()
+      |> Map.get(:query)
+      |> URI.decode_query()
+      |> Map.get("oobCode")
     end
   end
 
@@ -78,14 +94,12 @@ defmodule FirebaseAdminEx.Auth do
     with {:ok, response} <-
            Request.request(
              :post,
-             "#{@auth_endpoint_account}#{project_id}/#{url_suffix}",
+             "#{@auth_endpoint_account}#{project_id(project_id)}/#{url_suffix}",
              payload,
              auth_header(client_email)
            ),
          {:ok, body} <- Response.parse(response) do
       {:ok, body}
-    else
-      {:error, error} -> raise Errors.ApiError, Kernel.inspect(error)
     end
   end
 
@@ -99,10 +113,31 @@ defmodule FirebaseAdminEx.Auth do
            ),
          {:ok, body} <- Response.parse(response) do
       {:ok, body}
+    end
+  end
+
+  defp do_request!(url_suffix, payload, client_email) do
+    with {:ok, response} <- do_request(url_suffix, payload, client_email) do
+      {:ok, response}
     else
       {:error, error} -> raise Errors.ApiError, Kernel.inspect(error)
     end
   end
+
+  defp do_request!(url_suffix, payload, client_email, project_id) do
+    with {:ok, response} <- do_request(url_suffix, payload, client_email, project_id)  do
+      {:ok, response}
+    else
+      {:error, error} -> raise Errors.ApiError, Kernel.inspect(error)
+    end
+  end
+
+  defp project_id(nil) do
+    {:ok, project_id} = Goth.Config.get(:project_id)
+    project_id
+  end
+
+  defp project_id(value), do: value
 
   defp auth_header(nil) do
     {:ok, token} = Goth.Token.for_scope(@auth_scope)
--- lib/response.ex
+++ lib/response.ex
@@ -1,5 +1,6 @@
 defmodule FirebaseAdminEx.Response do
   def parse(%HTTPoison.Response{} = response) do
+    IO.inspect response, lable: :response
     case response do
       %HTTPoison.Response{status_code: 200, body: body} ->
         {:ok, body}
